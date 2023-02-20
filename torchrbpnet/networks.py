fb99a707a56fa3b877495538c2ca935ac4f41d41
Marc Horlacher <marc.horlacher@gmail.com>
2023-02-20 15:16:43 -0500

    added linear projection to rna representation body

--- torchrbpnet/networks.py
+++ torchrbpnet/networks.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 # %%
-from .layers import Conv1DFirstLayer, Conv1DResBlock, IndexEmbeddingOutputHead
+from .layers import Conv1DFirstLayer, Conv1DResBlock, IndexEmbeddingOutputHead, LinearProjection
 
 # %%
 @gin.configurable()
@@ -15,6 +15,7 @@ class MultiRBPNet(nn.Module):
         self.n_tasks = n_tasks
 
         self.body = nn.Sequential(*[Conv1DFirstLayer(4, n_body_filters, 6)]+[(Conv1DResBlock(n_body_filters, n_body_filters, dilation=(2**i))) for i in range(n_layers)])
+        self.latent_rna_projection = LinearProjection(in_channels=n_body_filters)
         self.head = IndexEmbeddingOutputHead(self.n_tasks, dims=n_body_filters)
     
     def forward(self, inputs, **kwargs):
