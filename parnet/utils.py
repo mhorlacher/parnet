0f9ec2331ad2da095e5db9280e43f9aab3b74e21
marc.horlacher <marc.horlacher@bayer.com>
2025-06-11 12:37:27 +0200

    removed remnants of tensorflow

--- parnet/utils.py
+++ parnet/utils.py
@@ -1,36 +1,36 @@
 import os
 
-import tensorflow as tf
+# import tensorflow as tf
 import torch
 import torch.nn.functional as F
 
 
-def _disable_tensorflow_logs():
-    # Disable tensorflow INFO and WARNING log messages. This needs to be
-    # done *before* tensorflow is imported.
-    # TODO: Currently, we are also ignoring erros (levle=3) rather than
-    # just warning (level=2). This could be problematic in the future.
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# def _disable_tensorflow_logs():
+#     # Disable tensorflow INFO and WARNING log messages. This needs to be
+#     # done *before* tensorflow is imported.
+#     # TODO: Currently, we are also ignoring erros (levle=3) rather than
+#     # just warning (level=2). This could be problematic in the future.
+#     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-    # %%
-    # Disable absl INFO and WARNING log messages
-    from absl import logging as absl_logging
+#     # %%
+#     # Disable absl INFO and WARNING log messages
+#     from absl import logging as absl_logging
 
-    absl_logging.set_verbosity(absl_logging.ERROR)
+#     absl_logging.set_verbosity(absl_logging.ERROR)
 
 
-# %%
-def _set_tf_dynamic_memory_growth():
-    import tensorflow as tf
+# # %%
+# def _set_tf_dynamic_memory_growth():
+#     import tensorflow as tf
 
-    # Make sure that tensorflow doesn't gobble up all GPU memory
-    physical_devices = tf.config.list_physical_devices('GPU')
-    try:
-        for device in physical_devices:
-            tf.config.experimental.set_memory_growth(device, True)
-    except:
-        # Invalid device or cannot modify virtual devices once initialized.
-        pass
+#     # Make sure that tensorflow doesn't gobble up all GPU memory
+#     physical_devices = tf.config.list_physical_devices('GPU')
+#     try:
+#         for device in physical_devices:
+#             tf.config.experimental.set_memory_growth(device, True)
+#     except:
+#         # Invalid device or cannot modify virtual devices once initialized.
+#         pass
 
 
 # %%
@@ -64,9 +64,9 @@ def sparse_to_dense(indices, values, size):
     return torch.sparse_coo_tensor(indices, values, size).to_dense().to(torch.float32)
 
 
-def sample_to_torch_sparse_tensor_dict(example):
-    return {
-        'meta': {'name': example['meta']['name'].numpy()},
-        'inputs': tf.nest.map_structure(lambda x: to_sparse_tensor_dict(torch.tensor(x.numpy())), example['inputs']),
-        'outputs': tf.nest.map_structure(lambda x: to_sparse_tensor_dict(torch.tensor(x.numpy())), example['outputs']),
-    }
+# def sample_to_torch_sparse_tensor_dict(example):
+#     return {
+#         'meta': {'name': example['meta']['name'].numpy()},
+#         'inputs': tf.nest.map_structure(lambda x: to_sparse_tensor_dict(torch.tensor(x.numpy())), example['inputs']),
+#         'outputs': tf.nest.map_structure(lambda x: to_sparse_tensor_dict(torch.tensor(x.numpy())), example['outputs']),
+#     }
