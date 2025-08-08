31de0ce327b156f1c54045a2ebd9ce332f43ea5f
marc.horlacher <marc.horlacher@bayer.com>
2025-08-08 09:33:52 +0200

    implemented Samba

--- parnet/models.py
+++ parnet/models.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 import tqdm
@@ -6,9 +7,61 @@ import torch
 import torch.nn as nn
 import captum
 
+from fla.models.samba import SambaConfig, SambaModel
+
 
 from parnet.utils import sequence_to_onehot
 from parnet.layers import StemConv1D, ResConvBlock1D, AdditiveMix
+
+
+@gin.configurable()
+class RBPNetBackbone(nn.Module):
+    def __init__(self, dilation: float = 1.75, layers: int = 9, embedding_dim: int = 128):
+        super().__init__()
+        self.layers = nn.Sequential(*[ResConvBlock1D(dilation=int(dilation**i)) for i in range(layers)])
+
+        # final linear projection layer to produce sequence embeddings
+        self.projection = nn.LazyConv1d(embedding_dim, kernel_size=1, bias=False, padding='same')
+
+    def forward(self, x: torch.Tensor):
+        return self.projection(self.layers(x))
+
+
+@gin.configurable()
+class SambaBackbone(SambaModel):
+    def __init__(
+        self, num_layers: int = 16, hidden_size: int = 128, attn_num_heads: int = 8, attn_window_size: int = 256
+    ):
+        # create config based on args (so it's assignable via gin)
+        assert hidden_size % attn_num_heads == 0, 'hidden_size must be divisible by attn_num_heads'
+        config = SambaConfig(
+            hidden_size=hidden_size,
+            num_hidden_layers=num_layers,
+        )
+        config.attn['window_size'] = attn_window_size
+        config.attn['num_heads'] = attn_num_heads
+        config.attn['num_kv_heads'] = attn_num_heads
+        config.max_position_embeddings = attn_window_size
+
+        # init the model with the config
+        super().__init__(config)
+
+        # remove the embeddings (unused)
+        del self.embeddings
+
+        # set to half-precision (required for flash-attention)
+        self.half()
+
+    def forward(self, x):
+        # need to switch dimension: (batch_size, num_channels, sequence_length) -> (batch_size, sequence_length, num_channels)
+        x_org_device = x.device
+        return (
+            super()
+            .forward(inputs_embeds=x.transpose(-2, -1).half().to(self.device), use_cache=False)
+            .last_hidden_state.float()
+            .transpose(-2, -1)
+            .to(x_org_device)
+        )
 
 
 @gin.configurable()
@@ -18,12 +71,9 @@ class RBPNet(nn.Module):
     def __init__(
         self,
         num_tasks: int = None,
-        layers: int = 9,
-        dilation: int = 1.75,
         stem_layer: nn.Module = StemConv1D,
-        body_layer: nn.Module = ResConvBlock1D,
+        backbone: nn.Module = RBPNetBackbone(),
         head_layer: nn.Module = AdditiveMix,
-        embedding_dim: int = 128,
     ):
         """Initializes RBPNet.
 
@@ -40,10 +90,7 @@ class RBPNet(nn.Module):
             raise ValueError('num_tasks must be specified in the gin config file.')
 
         self.stem = stem_layer()
-        self.body = nn.Sequential(*[body_layer(dilation=int(dilation**i)) for i in range(layers)])
-
-        # final linear projection layer to produce sequence embeddings
-        self.projection = nn.LazyConv1d(embedding_dim, kernel_size=1, bias=False, padding='same')
+        self.backbone = backbone
 
         if head_layer is None:
             raise ValueError('head_layer must be specified.')
@@ -52,6 +99,8 @@ class RBPNet(nn.Module):
         # Dummy forward pass to initialize weights. Not strictly required, but allows us
         # to print a proper summary of the model with pytorch_lightning and get the correct
         # number of parameters.
+        if isinstance(self.backbone, SambaBackbone):
+            self.backbone = self.backbone.to('cuda')
         _ = self(torch.zeros(2, 4, 100, dtype=torch.float32))
 
     def forward(self, sequence: torch.Tensor, to_probs=False) -> dict[str, torch.Tensor]:
@@ -65,9 +114,7 @@ class RBPNet(nn.Module):
             dict[str, torch.Tensor]: A dictionary logits for total, target, control tracks and the mixing coefficient.
         """
         x = self.stem(sequence)
-        x = self.body(x)
-        x = self.projection(x)
-
+        x = self.backbone(x)
         x = self.head(x)
 
         if isinstance(x, torch.Tensor):
